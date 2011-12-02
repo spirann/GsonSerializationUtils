4a4e1d9a73debd2ef8128b8b41052e1c5f367b62
Simon <spireyn@gmail.com>
2011-12-02 11:36:18 +0100

    fixed annotations

--- src/main/java/com/gsu/annotations/Simplified.java
+++ src/main/java/com/gsu/annotations/Simplified.java
@@ -1,5 +1,9 @@
 package com.gsu.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(value= RetentionPolicy.RUNTIME)
 public @interface Simplified {
 
 }
--- src/main/java/com/gsu/tools/GsonBuilderFactory.java
+++ src/main/java/com/gsu/tools/GsonBuilderFactory.java
@@ -15,43 +15,40 @@ public class GsonBuilderFactory {
     public static GsonBuilder getGsonBuilder(final Class<?> clazz) {
         GsonBuilder builder = new GsonBuilder();
         if(!isPrimitive(clazz)){
-            System.out.println("**** "+clazz);
             builder.registerTypeAdapter(clazz, new JsonSerializer(){
 
                 @Override
                 public JsonElement serialize(Object src, Type typeOfSrc, JsonSerializationContext context){
                     JsonObject object = new JsonObject();
                     for(Field field : src.getClass().getDeclaredFields()){
-                        System.out.println("type "+field.getType());
+                        field.setAccessible(true);
                         try{
-                            if(! isPrimitive(field)){
-                                if (field.isAnnotationPresent(Simplified.class)){
-                                    //if(field.getType().equals(Collection.class)) {}
-                                    //else if(field.getType() instanceof Map<?,?>) {}
-                                    field.setAccessible(true);
-                                    JsonElement reference = getBasicGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
-                                    object.add(field.getName(), reference);
-                                } else if (field.isAnnotationPresent(Primitive.class)){
-                                    field.setAccessible(true);
-                                    JsonElement id = context.serialize(field.get(src).toString());
-                                    object.add(field.getName(), id);
+                            if (field.get(src)!= null){
+                                if(!isPrimitive(field.get(src))){
+                                    if (field.isAnnotationPresent(Simplified.class)){
+                                        //if(field.getType().equals(Collection.class)) {}
+                                        //else if(field.getType() instanceof Map<?,?>) {}
+                                        JsonElement reference = getBasicGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
+                                        object.add(field.getName(), reference);
+                                    } else if (field.isAnnotationPresent(Primitive.class)){
+                                        addPrimitive(src,object,field);
+                                    } else {
+                                        JsonElement element = getGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
+                                        object.add(field.getName(), element);
+                                    }
                                 } else {
-                                    field.setAccessible(true);
-                                    JsonElement element = getGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
-                                    object.add(field.getName(), element);
+                                    addPrimitive(src,object,field);
                                 }
                             }
                         } catch(Exception e){
                             e.printStackTrace();
                         }
-
                     }
                     return object;
                 }
 
             });
         }
-        
         return builder;
     }
 
@@ -61,24 +58,24 @@ public class GsonBuilderFactory {
         GsonBuilder builder = new GsonBuilder();
         
         if(!isPrimitive(clazz)){
-            System.out.println(clazz);
             builder.registerTypeAdapter(clazz, new JsonSerializer(){
 
                 @Override
                 public JsonElement serialize(Object src, Type typeOfSrc, JsonSerializationContext context){
-
                     JsonObject object = new JsonObject();
                     for(Field field : src.getClass().getDeclaredFields()){
                         try {
-                            if(! isPrimitive(field.get(src))){
-                                if (field.isAnnotationPresent(Primitive.class)){
-                                    field.setAccessible(true);
-                                    JsonElement id = context.serialize(field.get(src).toString());
-                                    object.add(field.getName(), id);
-                                } else if ( !field.isAnnotationPresent(Simplified.class)){
-                                    field.setAccessible(true);
-                                    JsonElement element = getBasicGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
-                                    object.add(field.getName(), element);
+                            field.setAccessible(true);
+                            if (field.get(src)!= null){
+                                if(! isPrimitive(field.get(src))){
+                                    if (field.isAnnotationPresent(Primitive.class)){
+                                        addPrimitive(src,object,field);
+                                    } else if (!field.isAnnotationPresent(Simplified.class)){
+                                        JsonElement element = getBasicGsonBuilder(field.getType()).create().toJsonTree(field.get(src));
+                                        object.add(field.getName(), element);
+                                    }
+                                } else {
+                                    addPrimitive(src,object,field);
                                 }
                             }
                         } catch (Exception e) {
@@ -87,13 +84,14 @@ public class GsonBuilderFactory {
                     }
                     return object;
                 }
+
             });
         }
         return builder;
     }
 
     private static boolean isPrimitive(Object object){
-          return object.getClass().isPrimitive()||
+        return object.getClass().isPrimitive()||
                  Number.class.isInstance(object)||
                  Boolean.class.isInstance(object)||
                  String.class.isInstance(object);
@@ -105,14 +103,9 @@ public class GsonBuilderFactory {
                   String.class.isAssignableFrom(clazz);
     }
 
-    private static void addPrimitive(Object src, JsonObject object, Field field) {
-        try{
-            field.setAccessible(true);
+    private static void addPrimitive(Object src, JsonObject object, Field field) throws IllegalAccessException {
             JsonElement primitive = new JsonPrimitive(field.get(src).toString());
             object.add(field.getName(), primitive);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
     }
     
     
